bc364a3e692fb954d780908f0941d97842a884db
iceblackhot <iceblackhot2@gmail.com>
2022-07-08 18:01:28 +0300

    add-custom-scroll+filters

--- src/components/filters/filters.jsx
+++ src/components/filters/filters.jsx
@@ -2,5 +2,65 @@ import React from 'react';
 import './filters.scss';
 
 export const Filters = () => {
-  return <div className="search-filters"></div>;
+  return (
+    <div className="search-filters">
+      <div className="search-filters__container">
+        <div className="search-filters__header">
+          <form>
+            <input placeholder="Город" className="search-filters__input" type="text" />
+            <button>Сброс</button>
+          </form>
+        </div>
+        <div className="search-filters__viewport">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum qui laboriosam odit
+            explicabo a, error vero praesentium esse harum iusto, nulla rem sint voluptas
+            exercitationem ullam! Corporis iusto eligendi maxime?Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Earum qui laboriosam odit explicabo a, error vero
+            praesentium esse harum iusto, nulla rem sint voluptas exercitationem ullam! Corporis
+            iusto eligendi maxime?Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum qui
+            laboriosam odit explicabo a, error vero praesentium esse harum iusto, nulla rem sint
+            voluptas exercitationem ullam! Corporis iusto eligendi maxime?Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Earum qui laboriosam odit explicabo a, error vero
+            praesentium esse harum iusto, nulla rem sint voluptas exercitationem ullam! Corporis
+            iusto eligendi maxime?Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum qui
+            laboriosam odit explicabo a, error vero praesentium esse harum iusto, nulla rem sint
+            voluptas exercitationem ullam! Corporis iusto eligendi maxime?Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Earum qui laboriosam odit explicabo a, error vero
+            praesentium esse harum iusto, nulla rem sint voluptas exercitationem ullam! Corporis
+            iusto eligendi maxime?Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum qui
+            laboriosam odit explicabo a, error vero praesentium esse harum iusto, nulla rem sint
+            voluptas exercitationem ullam! Corporis iusto eligendi maxime?
+          </p>
+        </div>
+      </div>
+      <div className="search-filters__container">
+        <div className="search-filters__header">
+          <form>
+            <input placeholder="Улица" className="search-filters__input" type="text" />
+            <button>Сброс</button>
+          </form>
+        </div>
+        <div className="search-filters__viewport"></div>
+      </div>
+      <div className="search-filters__container">
+        <div className="search-filters__header">
+          <form>
+            <input placeholder="Статус" className="search-filters__input" type="text" />
+            <button>Сброс</button>
+          </form>
+        </div>
+        <div className="search-filters__viewport"></div>
+      </div>
+      <div className="search-filters__container">
+        <div className="search-filters__header">
+          <form>
+            <input placeholder="Сотрудник" className="search-filters__input" type="text" />
+            <button>Сброс</button>
+          </form>
+        </div>
+        <div className="search-filters__viewport"></div>
+      </div>
+    </div>
+  );
 };
